9060efc761e8b88446636d5942e25cde11f3c4b5
polyhistor2050 <chrisspaul2050@gmail.com>
2023-03-16 20:21:09 +0300

    updates to the file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let food = ["Pizza", "Burger", "Pasta", "Omlett", "Sausage", "Sandwich"];
 let foodOrdered = 0;
-setTimeout(popUp, 5000);
+setTimeout(popUp, 1000);
 menu = true;
 function popUp() {
 		let orderList = prompt("WELCOME TO OUR MENU (Please place your order:");
@@ -8,13 +8,16 @@ function popUp() {
 			if (food[i].toLowerCase() == orderList.toLowerCase()){
 				foodOrdered = foodOrdered + 1;
 				alert(`You Ordered ${food[i]}`);
-				break;
+				welc();
+				return;
 			}
 		}
 		alert("Sorry.., That food is not available in our menu");
 	}
-		
-if(foodOrdered == 1){
-	menu = true;
-	alert("Thanks for using our app, Pleasure to serve you");
+function welc() {
+	if(foodOrdered == 1){
+		menu = true;
+		alert("Thanks for using our app, Pleasure to serve you");
+	}
 }
+
